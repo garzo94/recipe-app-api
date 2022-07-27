5e739a7aa2e94c4f190b628b4e1c86455f5545a5
garzo94 <alexgarzo25@gmail.com>
2022-07-27 16:38:25 -0600

    final project

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,12 +1,12 @@
 """
 Views for the recipe APIs.
 """
-# from drf_spectacular.utils import (
-#     extend_schema_view,
-#     extend_schema,
-#     OpenApiParameter,
-#     OpenApiTypes,
-# )
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 from rest_framework import (
     viewsets,
     mixins,
@@ -26,22 +26,22 @@ from core.models import (
 from recipe import serializers
 
 
-# @extend_schema_view(
-#     list=extend_schema(
-#         parameters=[
-#             OpenApiParameter(
-#                 'tags',
-#                 OpenApiTypes.STR,
-#                 description='Comma separated list of IDs to filter',
-#             ),
-#             OpenApiParameter(
-#                 'ingredients',
-#                 OpenApiTypes.STR,
-#                 description='Comma separated list of ingredient IDs to filter',
-#             )
-#         ]
-#     )
-# )
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of IDs to filter',
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter',
+            )
+        ]
+    )
+)
 class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeDetailSerializer
@@ -49,21 +49,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    # def _params_to_ints(self, qs):
-    #     """Convert a list of strings to integers."""
-    #     return [int(str_id) for str_id in qs.split(',')]
+    def _params_to_ints(self, qs):
+        """Convert a list of strings to integers."""
+        return [int(str_id) for str_id in qs.split(',')]
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
-        # tags = self.request.query_params.get('tags')
-        # ingredients = self.request.query_params.get('ingredients')
-        # queryset = self.queryset
-        # if tags:
-        #     tag_ids = self._params_to_ints(tags)
-        #     queryset = queryset.filter(tags__id__in=tag_ids)
-        # if ingredients:
-        #     ingredient_ids = self._params_to_ints(ingredients)
-        #     queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
 
         return self.queryset.filter(
             user=self.request.user
@@ -95,17 +95,17 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# @extend_schema_view(
-#     list=extend_schema(
-#         parameters=[
-#             OpenApiParameter(
-#                 'assigned_only',
-#                 OpenApiTypes.INT, enum=[0, 1],
-#                 description='Filter by items assigned to recipes.',
-#             )
-#         ]
-#     )
-# )
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            )
+        ]
+    )
+)
 class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
                             mixins.UpdateModelMixin,
                             mixins.ListModelMixin,
@@ -116,17 +116,15 @@ class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Filter querset to authenticated user."""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-        # assigned_only = bool(
-        #     int(self.request.query_params.get('assigned_only', 0))
-        # )
-    #     queryset = self.queryset
-    #     if assigned_only:
-    #         queryset = queryset.filter(recipe__isnull=False)
 
-    #     return queryset.filter(
-    #         user=self.request.user
-    #     ).order_by('-name').distinct()
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(user=self.request.user).order_by('-name') # sesion 17
 
 
 class TagViewSet(BaseRecipeAttrViewSet):
